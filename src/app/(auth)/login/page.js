0b63c61e980a860dc071da2ccb1aed414b44d740
boi <lorem4690@gmail.com>
2024-05-15 20:57:22 +0700

    fix: add navbar to login and register page

--- src/app/(auth)/login/page.js
+++ src/app/(auth)/login/page.js
@@ -3,20 +3,24 @@
 import AuthComponent from '@/components/AuthComponent';
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
+import {Navbar} from "@/components/Navbar";
 
 const Login = () => {
   const router = useRouter();
 
   return (
-    <div className='hero min-h-screen bg-base-200'>
-      <div className='hero-content flex-col lg:flex-row-reverse'>
-        <AuthComponent isLogin={true} />
+      <>
+          <Navbar/>
+          <div className='hero min-h-screen bg-base-200'>
+              <div className='hero-content flex-col lg:flex-row-reverse'>
+                  <AuthComponent isLogin={true} />
 
-        <div className='text-center lg:text-left invisible lg:visible'>
-          <Image src='/images/Hero-image.png' className='max-w-[300px] rounded-lg' alt='' width={300} height={300} />
-        </div>
-      </div>
-    </div>
+                  <div className='text-center lg:text-left invisible lg:visible'>
+                      <Image src='/images/Hero-image.png' className='max-w-[300px] rounded-lg' alt='' width={300} height={300} />
+                  </div>
+              </div>
+          </div></>
+
   );
 };
 export default Login;
--- src/app/(auth)/register/page.js
+++ src/app/(auth)/register/page.js
@@ -3,21 +3,27 @@
 import AuthComponent from '@/components/AuthComponent';
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
+import {Navbar} from "@/components/Navbar";
 
 const Register = () => {
   const router = useRouter();
 
   
   return (
-    <div className='hero min-h-screen bg-base-200'>
-      <div className='hero-content flex-col lg:flex-row-reverse'>
-        <AuthComponent isLogin={false} />
+      <>
+          <Navbar/>
+          <div className='hero min-h-screen bg-base-200'>
+              <div className='hero-content flex-col lg:flex-row-reverse'>
+                  <AuthComponent isLogin={false}/>
 
-        <div className='text-center lg:text-left invisible lg:visible'>
-          <Image src='/images/Hero-image.png' className='max-w-[300px] rounded-lg' alt='' width={300} height={300} />
-        </div>
-      </div>
-    </div>
+                  <div className='text-center lg:text-left invisible lg:visible'>
+                      <Image src='/images/Hero-image.png' className='max-w-[300px] rounded-lg' alt='' width={300}
+                             height={300}/>
+                  </div>
+              </div>
+          </div>
+      </>
+
   );
 };
 
