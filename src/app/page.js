dc77c7f6e3f4819629390f116c2a23e5a4ded44e
Monik-star <monikariin@gmail.com>
2024-05-17 17:22:06 +0700

    adjust landing page

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,14 @@
 import { LandingPage } from '@/components/LandingPage';
 import { Navbar } from '@/components/Navbar';
+import getConfig from 'next/config';
+
+const { serverRuntimeConfig } = getConfig();
 
 export default async function Home() {
   return (
     <>
       <Navbar />
-      <LandingPage />
+      <LandingPage config={serverRuntimeConfig} />
     </>
   );
 }
